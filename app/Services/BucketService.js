52244aebf2e6c8aed756cc8d5ce42518bae49709
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2019-10-17 11:36:09 -0300

    chore(refator): para métodos estaticos

--- app/Services/BucketService.js
+++ app/Services/BucketService.js
@@ -4,19 +4,19 @@ const fs = require('fs')
 const Config = use('Config')
 
 class BucketService {
-  constructor () {
-    this.bucket = Config.get('sqlgrid.bucket')
+  static get bucket () {
+    return Config.get('sqlgrid.bucket')
   }
 
   // e.g, await BucketService.writeFile(pathname, filename)
-  writeFile (pathname, filename) {
+  static writeFile (pathname, filename) {
     if (!pathname && !filename) return
 
     return new Promise((resolve, reject) => {
       try {
         const buffer = fs.readFileSync(pathname)
-        this.writeFile = this.bucket.writeFile({ filename, buffer })
-        resolve(this.writeFile)
+        const writeFile = BucketService.bucket.writeFile({ filename, buffer })
+        resolve(writeFile)
       } catch (error) {
         reject(error)
       }
@@ -24,13 +24,13 @@ class BucketService {
   }
 
   // e.g, await BucketService.readFile({ id })
-  readFile (spec) {
+  static readFile (spec) {
     if (!spec) return
 
     return new Promise((resolve, reject) => {
       try {
-        this.readFile = this.bucket.readFile(spec)
-        resolve(this.readFile)
+        const readFile = BucketService.bucket.readFile(spec)
+        resolve(readFile)
       } catch (error) {
         reject(error)
       }
@@ -38,12 +38,12 @@ class BucketService {
   }
 
   // e.g, await BucketService.createFile({ id }, pathname)
-  createFile (spec, pathname) {
+  static createFile (spec, pathname) {
     if (!spec && !pathname) return
 
     return new Promise((resolve, reject) => {
       try {
-        const readStream = this.bucket.createReadStream(spec)
+        const readStream = BucketService.bucket.createReadStream(spec)
         readStream.pipe(fs.createWriteStream(pathname))
         resolve()
       } catch (error) {
@@ -53,4 +53,4 @@ class BucketService {
   }
 }
 
-module.exports = new BucketService()
+module.exports = BucketService
