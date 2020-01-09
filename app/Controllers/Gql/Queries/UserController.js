d234ae43002dc54d24bae0c521576ded12dca20a
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2020-01-09 17:33:29 -0300

    feat: Adicionada consulta de usuários
    Criado método users que permite consultar a partir de um uuid de
    organização ou o de um usuário específico

--- app/Controllers/Gql/Queries/UserController.js
+++ app/Controllers/Gql/Queries/UserController.js
@@ -1,7 +1,44 @@
 'use strict'
 
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Organization = use('App/Models/Organization')
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const User = use('App/Models/User')
+
 class UserController {
-  async users (parent, arg, ctx) {}
+  async users (parent, arg, ctx) {
+    const {
+      organization = {},
+      user = {},
+      page = 1,
+      perPage = 10
+    } = arg
+
+    if (organization.uuid) {
+      const o = await Organization
+        .findByOrFail('uuid', organization.uuid)
+      const users = await o
+        .users()
+        .with('avatar', avatar => {
+          avatar.setVisible(['avatar'])
+        })
+        .paginate(page, perPage)
+      return users.toJSON()
+    }
+
+    if (user.uuid) {
+      const u = await User
+        .findByOrFail('uuid', user.uuid)
+      return {
+        perPage,
+        page,
+        lastPage: 1,
+        total: 1,
+        data: [u.toJSON()]
+      }
+    }
+  }
 }
 
 module.exports = UserController
