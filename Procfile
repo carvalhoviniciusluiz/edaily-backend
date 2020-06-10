00a1c0392509a630ca33226bce486f4453a1e126
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2020-06-10 09:44:42 -0300

    feat: Ajustando leitura das variaveis ambiente

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
-web: npm start
-worker: npm run dev:queue
+release: ENV_SILENT=true node ace migration:run --force
+web: ENV_SILENT=true npm start
+worker: ENV_SILENT=true npm run dev:queue
--- config/database.js
+++ config/database.js
@@ -1,10 +1,14 @@
 'use strict'
 
+const Url = require('url-parse')
+
 /** @type {import('@adonisjs/framework/src/Env')} */
 const Env = use('Env')
 
 /** @type {import('@adonisjs/ignitor/src/Helpers')} */
 const Helpers = use('Helpers')
+
+const DATABASE_URL = new Url(Env.get('DATABASE_URL')) || {}
 
 module.exports = {
   /*
@@ -51,11 +55,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
+      host: Env.get('DB_HOST', DATABASE_URL.hostname || 'localhost'),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      user: Env.get('DB_USER', DATABASE_URL.username || 'root'),
+      password: Env.get('DB_PASSWORD', DATABASE_URL.password || ''),
+      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1) || 'adonis')
     },
     debug: Env.get('DB_DEBUG', false)
   },
@@ -73,11 +77,11 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
+      host: Env.get('DB_HOST', DATABASE_URL.hostname || 'localhost'),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
-      password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      user: Env.get('DB_USER', DATABASE_URL.username || 'root'),
+      password: Env.get('DB_PASSWORD', DATABASE_URL.password || ''),
+      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1) || 'adonis')
     },
     debug: Env.get('DB_DEBUG', false)
   }
